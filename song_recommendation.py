9bd2c9a45c545bca3ef09e79d09a36542a0cb7fb
evannguyenprog <evanalexnguyen@gmail.com>
2023-11-13 18:50:01 -0800

    search broadening and artist selection
    added option to search by title and artist, or just title. added top 5 options to select from

--- song_recommendation.py
+++ song_recommendation.py
@@ -1,20 +1,51 @@
-import spotipy 
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '724086c08ef04c05a8b4b024d59ef0e3'
-client_secret = '0bcf065450cd47289b378b98f8ba0533'
+# Replace these with your own values
+client_id = 'your_client_id'
+client_secret = 'your_client_secret'
 
+# Set up the Spotify API client
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_song_info(track_title, artist):
+def search_song(title, artist=None):
     try:
+        # Construct the query based on whether the artist is provided
+        if artist:
+            query = f"track:{title} artist:{artist}"
+        else:
+            query = f"track:{title}"
 
-        query = f"{track_title} artist:{artist}"
-        results = sp.search(q=query, type='track', limit=1)
-        track = results['tracks']['items'][0]
-        audio_features = sp.audio_features([track['id']])[0]
+        # Use the search endpoint to look for potential matches
+        results = sp.search(q=query, type='track', market='US', limit=5)
 
+        # Extract relevant information from the results
+        potential_results = []
+        for track in results['tracks']['items']:
+            potential_results.append({
+                'track_name': track['name'],
+                'artist': track['artists'][0]['name'],
+                'album': track['album']['name'],
+                'release_date': track['album']['release_date'],
+                'id': track['id'],
+            })
+
+        return potential_results
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def get_song_info(track_id):
+    try:
+        # Get track details using the track ID
+        track = sp.track(track_id)
+
+        # Get audio features including BPM
+        audio_features = sp.audio_features([track_id])[0]
+
+        # Extract relevant information
         song_info = {
             'track_name': track['name'],
             'artist': track['artists'][0]['name'],
@@ -26,22 +57,37 @@ def get_song_info(track_title, artist):
         }
 
         return song_info
+
     except Exception as e:
         print(f"Error: {e}")
         return None
-    
 
 if __name__ == "__main__":
-    
-    track_name = input("Enter song: ")
-    artist = input("Enter the artist: ")
-    
+    # Prompt the user for the song title and optional artist
+    track_title = input("Enter the title of the song: ")
+    artist = input("Enter the artist (press Enter to skip): ")
 
-    song_info = get_song_info(track_name, artist)
+    # Search for potential matches
+    potential_results = search_song(track_title, artist)
 
-    if song_info:
-        print("\nSong Information:")
-        for key, value in song_info.items():
-            print(f"{key}: {value}")
+    if potential_results:
+        print("\nPotential Matches:")
+        for i, result in enumerate(potential_results, start=1):
+            print(f"{i}. {result['track_name']} by {result['artist']}")
+
+        choice = input("Enter the number of the desired match (or press Enter to exit): ")
+
+        if choice.strip() and choice.isdigit() and 1 <= int(choice) <= len(potential_results):
+            selected_track_id = potential_results[int(choice) - 1]['id']
+
+            # Retrieve detailed information for the selected track
+            song_info = get_song_info(selected_track_id)
+
+            if song_info:
+                print("\nSong Information:")
+                for key, value in song_info.items():
+                    print(f"{key}: {value}")
+            else:
+                print("\nFailed to retrieve detailed song information.")
     else:
-        print("Failed to retrieve song information.")
+        print("\nNo potential matches found.")
